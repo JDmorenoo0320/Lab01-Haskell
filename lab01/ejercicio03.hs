347d619133efe2d33a7a11d81e5972160a1221d9
unknown <juan.mdaleman@labinfo.is.escuelaing.edu.co>
2026-03-09 09:23:27 -0500

    feature: ejercicio 3 terminado

--- lab01/ejercicio03.hs
+++ lab01/ejercicio03.hs
@@ -7,3 +7,32 @@ cuadrado a = a ^ 2
 sumaCuadrados a b = cuadrado a + cuadrado b
 
 --Output: 13
+
+--b)
+main = print(dobleLista [1,2,3,4])
+
+dobleLista a = map (*2) a
+
+--Output: [2,4,6,8]
+
+--2) 
+--a) 
+main = print(clasificarNota 2)
+
+clasificarNota a
+            | a < 3          = "Reprobo"
+            | a < 4          = "En recuperacion"
+            | a >= 4         = "Aprobo"
+
+-- Output: "Reprobo"
+
+--b)
+main = print(fibonacci 12)
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci 2 = 1
+fibonacci a = fibonacci (a-1) + fibonacci (a-2)
+
+--Output: 144
+
